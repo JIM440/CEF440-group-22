eb2ecd3465f90b38f10a09f97c69c23b7f63d65a
JIM440 <takemjim43@gmail.com>
2024-07-05 08:58:56 +0100

    static structure of guides added

--- source_code/src/_citizens/pages/Guides/Guides.tsx
+++ source_code/src/_citizens/pages/Guides/Guides.tsx
@@ -12,7 +12,7 @@ import {
   import React from 'react';
   import fire from '../../../assets/images/fire.png';
   import eruption from '../../../assets/images/volcano.png';
-  import landslide from '../../../assets/images/landslide.webp';
+  import landslide from '../../../assets/images/landslide.png';
   import tornado from '../../../assets/images/tornado.webp';
   import earthquake from '../../../assets/images/earthquake.png';
   import flood from '../../../assets/images/flood.png';
@@ -26,37 +26,37 @@ import BackBtn from '../../../components/HeaderBack';
       {
         image: tornado,
         name: 'Tornado',
-        title: 'Tornado Survival',
+        title: 'Tornado',
         description: 'Prepare for tornadoes with this detailed guide, which covers safety measures and recovery strategies.'
       },
       {
         image: fire,
         name: 'Fire',
-        title: 'Fire Safety Manual',
+        title: 'Fire',
         description: 'Stay safe and informed with this comprehensive guide on fire preparedness, response, and recovery.',
       },
       {
         image: eruption,
-        name: 'Volcanic',
-        title: 'Eruptions Guide',
+        name: 'Eruption',
+        title: 'Eruptions',
         description: 'Be ready for volcanic eruptions with this comprehensive guide, covering essential steps for safety and recovery.',
       },
       {
         image: landslide,
         name: 'Landslide',
-        title: 'Landslide Readiness',
+        title: 'Landslide',
         description: 'Learn about the actions to take before, during, and after a landslide to protect yourself and your community.'
       },
       {
         image: flood,
         name: 'Flood',
-        title: 'Flood Survival Guide',
+        title: 'Flood',
         description: 'This guide provides information on how to prepare for, respond to, and recover from floods.',
       },
       {
         image: earthquake,
         name: 'Earthquake',
-        title: 'Earthquake Guide',
+        title: 'Earthquake',
         description: 'Discover important tips and strategies for earthquake preparedness, response, and recovery.'
       },
     ];
@@ -69,6 +69,9 @@ import BackBtn from '../../../components/HeaderBack';
         </IonHeader>
   
         <IonContent className="ion-padding">
+
+<p style={{textAlign: 'center'}}>Know exactly how to prepare, respond, and recover from disasters using the guides below.</p>
+
           <div className="guide-list-containers">
             {Guides.map((guide, index) => (
               <div key={index}>              
@@ -77,10 +80,10 @@ import BackBtn from '../../../components/HeaderBack';
                   <img src={guide.image} alt="" />
                   <div className="guide-content">
                     <h4>{guide.title}</h4>
-                    <p>{guide.description}</p>
+                    {/* <p>{guide.description}</p> */}
                    <div className="btn-chevron-container">
                    <button onClick={() => {
-                  router.push(`/menu/guides/${guide.name}`);
+                  router.push(`/menu/guides/${guide.name.toLowerCase()}`);
                 }}>Learn More </button><IonIcon color='primary' icon={chevronForward} />
                    </div>
                   </div>
